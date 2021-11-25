a989ec139552e6b71ef05381c9a08e3aae5973d2
lucas-git21 <estagiariolucasjf21@gmail.com>
2021-11-25 16:54:26 -0300

    incluindo nome de usuario

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ let input1 = document.getElementById('data_reserva_1')
 let input2 = document.getElementById('data_reserva_2')
 let input3 = document.getElementById('data_reserva_3')
 
-function reserva(){
+//botão "Pré-reserva"
+function reserva() {
     input1 = document.getElementById('data_reserva_1')
     console.log(input1.value)
 
@@ -12,18 +13,22 @@ function reserva(){
     input3 = document.getElementById('data_reserva_3')
     console.log(input3.value)
 
-    alert('Pré-reserva feita!')
+    alert('Pré-reserva feita')
 }
 
-function consultar_reserva(){
-   let resp = document.getElementById('reservas_feitas')
+//botão Consultar reservas
+function consultar_reserva() {
+    let resp = document.getElementById('reservas_feitas')
+    let nomeUsuario = document.getElementById('usuario')
+    let testandoUsuario = ''
 
-   resp.innerHTML = `<h2>Reservas feitas por mim:</h2><br>`
-   resp.innerHTML += `<h3>Churrascaria</h3> <h4>Data: ${input1.value}</h4><br>`
-   resp.innerHTML += `<h3>Campo</h4> <h4>Data: ${input2.value}</h4><br>`
-   resp.innerHTML += `<h3>Piscina</h3> <h4>Data: ${input3.value}</h4>`
+    resp.innerHTML = `<h2>Minhas reservas</h2>`
+    resp.innerHTML += `<h3>Usuário: ${nomeUsuario.value}</h3><br>`
+    resp.innerHTML += `<h3>Churrascaria</h3> <h4>Data: ${input1.value}</h4><br>`
+    resp.innerHTML += `<h3>Campo</h4> <h4>Data: ${input2.value}</h4><br>`
+    resp.innerHTML += `<h3>Piscina</h3> <h4>Data: ${input3.value}</h4>`
 
-   input1.value = ''
-   input2.value = ''
-   input3.value = ''
+    input1.value = ''
+    input2.value = ''
+    input3.value = ''
 }
